205ecd60b71ef0a9f4325f339faaed8188423004
mgangl <mike.gangl@gmail.com>
2021-05-28 14:20:56 -0700

    added user-agent, bumped to 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='podaac-data-subscriber',
-      version='1.0',
+      version='1.1',
       description='PO.DAAC Data Susbcriber Command Line Tool',
       url='https://github.com/podaac/data-subscriber',
       author='PO.DAAC',
--- subscriber/podaac_data_subscriber.py
+++ subscriber/podaac_data_subscriber.py
@@ -18,7 +18,7 @@ import sys
 import argparse
 import datetime
 
-__version__ = "1.0"
+__version__ = "1.1"
 
 
 ## TODO: ???
@@ -100,6 +100,7 @@ def setup_earthdata_login_auth(endpoint):
     jar = CookieJar()
     processor = request.HTTPCookieProcessor(jar)
     opener = request.build_opener(auth, processor)
+    opener.addheaders = [('User-agent', 'podaac-subscriber-'+__version__)]
     request.install_opener(opener)
 
 ###############################################################################
