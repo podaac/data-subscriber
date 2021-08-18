2a036534178510ee3cc46e00d64e92684186a0c9
mgangl <mike.gangl@gmail.com>
2021-08-18 11:36:39 -0700

    added logging env vars to set log level for error resolution

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='podaac-data-subscriber',
-      version='1.1.1',
+      version='1.1.2',
       description='PO.DAAC Data Susbcriber Command Line Tool',
       url='https://github.com/podaac/data-subscriber',
       author='PO.DAAC',
