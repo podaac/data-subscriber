ed4c8c102131250b0acda890b7b970608305b40a
mgangl <gangl@jpl.caltech.edu>
2021-08-23 13:25:33 -0700

    Feature/ds flag improvements (#11)
    * added flake8 testing,. removed -ds flag, added start and end time
    
    * added flake8 ignore
    
    * added fix for no .update file found only looking back -m minutes. should be based on --start-date

--- test.py
+++ test.py
@@ -2,10 +2,17 @@ from subscriber import podaac_data_subscriber as pds
 import pytest
 
 
+def test_temporal_range():
+    assert pds.get_temporal_range(None,'2021-01-01T00:00:00Z',"2021-08-20T13:30:38Z") == "1900-01-01T00:00:00Z,2021-01-01T00:00:00Z"
+    assert pds.get_temporal_range('2021-01-01T00:00:00Z','2022-01-01T00:00:00Z',"2021-08-20T13:30:38Z") == "2021-01-01T00:00:00Z,2022-01-01T00:00:00Z"
+    assert pds.get_temporal_range('2021-01-01T00:00:00Z',None,"2021-08-20T13:30:38Z") == "2021-01-01T00:00:00Z,2021-08-20T13:30:38Z"
+    with pytest.raises(ValueError):
+        pds.get_temporal_range(None,None,None) == "2021-01-01T00:00:00Z,2021-08-20T13:30:38Z"
+
+
 def test_validate():
     #work
     a = validate(["-c", "viirs", "-d", "/data"])
-    assert a.dataSince == False
     assert a.collection == "viirs"
     assert a.outputDirectory == "/data"
 
@@ -14,9 +21,6 @@ def test_validate():
 
     a = validate(["-c", "viirs", "-d", "/data", "-b=-170,-80,170,20"])
     assert a.bbox == "-170,-80,170,20"
-
-    a = validate(["-c", "viirs", "-d", "/data", "-b=-180,-90,180,90", "-ds", "2021-01-01T00:00:00Z"])
-    assert a.dataSince == "2021-01-01T00:00:00Z"
 
     a = validate(["-c", "viirs", "-d", "/data", "-b=-180,-90,180,90", "-m","100"])
     assert a.minutes == 100, "should equal 100"
@@ -37,6 +41,25 @@ def test_validate():
     assert a.collection == "JASON_CS_S6A_L2_ALT_LR_STD_OST_NRT_F"
     assert a.outputDirectory == "/data"
     assert a.cycle is True
+
+    a = validate(["-c", "dataset", "-d", "/data", "-sd", "2020-01-01T00:00:00Z", "-ed", "2021-01-01T00:00:00Z"])
+    assert a.startDate == '2020-01-01T00:00:00Z'
+    assert a.endDate == '2021-01-01T00:00:00Z'
+
+    with pytest.raises(ValueError):
+        a = validate(["-c", "dataset", "-d", "/data", "-sd", "2020-01-01", "-ed", "2021-01-01T00:00:00Z"])
+
+    with pytest.raises(ValueError):
+        a = validate(["-c", "dataset", "-d", "/data", "-sd", "2020-01-01T00:00:00Z", "-ed", "2021-01-01"])
+
+    with pytest.raises(ValueError):
+        a = validate(["-c", "viirs", "-d", "/data", "-b=-180,-90,180,90anc", "-m","100"])
+
+    with pytest.raises(ValueError):
+        a = validate(["-c", "viirs", "-d", "/data", "-b=-180,-90,180,90,100", "-m","100"])
+
+    with pytest.raises(ValueError):
+        a = validate(["-c", "viirs", "-d", "/data", "-b=-180,-90,180", "-m","100"])
 
     # #don't work
     # with pytest.raises(SystemExit):
